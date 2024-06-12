5c3237021563a9aa6f103d283c512f2e46e2e41c
liuxin <sanjincoder777@gmail>
2024-06-12 16:10:56 +0800

    feat: 新增output配置

---
+++ 4.output配置/dist/output/index-CR2gkPhC.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const foo = (name) => {
+  console.log(`foo - ${name}`);
+};
+
+foo("rollup");
+//# sourceMappingURL=index-CR2gkPhC.js.map
---
+++ 4.output配置/dist/output/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const foo = (name) => {
+  console.log(`foo - ${name}`);
+};
+
+foo("rollup");
+//# sourceMappingURL=index.js.map
